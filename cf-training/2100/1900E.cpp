5b9604ef07420f627bf7df0902bd8edb54e9ad91
pinkyHead <1224430363@qq.com>
2025-05-25 14:10:31 +0800

    add targin

--- cf-training/2100/1900E.cpp
+++ cf-training/2100/1900E.cpp
@@ -13,9 +13,33 @@ typedef std::pair<ll, ll> pll;
 typedef double db;
 typedef long double ldb;
 const int maxn = 2e5 + 5;
-void tarjan(int u) {
-    
-}
+std::vector<int> e[maxn];
+std::vector<int> g[maxn];
+namespace tarjan {
+    int dfn[maxn], low[maxn], dfncnt, s[maxn], in_stack[maxn], tp;
+    int scc[maxn], sc;  // 结点 i 所在 SCC 的编号
+    int sz[maxn];       // 强连通 i 的大小
+
+    void tarjan(int u) {
+        low[u] = dfn[u] = ++dfncnt, s[++tp] = u, in_stack[u] = 1;
+        for (auto v : e[u]) {
+            if (!dfn[v]) {
+                tarjan(v);
+                low[u] = std::min(low[u], low[v]);
+            } else if (in_stack[v]) {
+                low[u] = std::min(low[u], dfn[v]);
+            }
+        }
+        if (dfn[u] == low[u]) {
+            ++sc;
+            do {
+                scc[s[tp]] = sc;
+                sz[sc]++;
+                in_stack[s[tp]] = 0;
+            } while (s[tp--] != u);
+        }
+    }
+};
 int main() {
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
@@ -27,6 +51,7 @@ int main() {
     int T = 1;
     std::cin >> T;
     while (T--) {
+        
     }
 
     return 0;
