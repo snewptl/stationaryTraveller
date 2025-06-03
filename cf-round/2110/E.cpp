b2dd27b7ee3945f3f9d3d7c8d7191223d323b166
何卓霖 <1224430363@qq.com>
2025-06-03 14:08:30 +0800

    update

--- cf-round/2110/E.cpp
+++ cf-round/2110/E.cpp
@@ -1,90 +1,72 @@
-#include <algorithm>
-#include <cstdio>
-#include <iostream>
-#include <map>
-#include <set>
-#include <queue>
-#include <vector>
-#include <assert.h>
-#define all(x) x.begin(), x.end()
-typedef long long ll;
-typedef std::pair<int, int> pii;
-typedef std::pair<ll, int> pli;
-typedef std::pair<ll, ll> pll;
-typedef double db;
-typedef long double ldb;
-const int maxn = 2e5 + 5;
-int n;
-int count;
-std::vector<std::set<int>> vec[2];
-std::map<int, int> mp;
-int ans[maxn];
-int vis[maxn];
-int a[2][maxn];
-bool solve() {
-    int sum[2] = {0, 0};
-    pii start = {0, 1};
-    for (int i = 0; i < 2; ++i) {
-        for (int j = 1; j <= count; ++j) {
-            sum[i] += vec[i][j].size() / 2;
-            if (vec[i][j].size() % 2 == 1) start = {i, j};
-        }
-    } 
-    if (sum[1] + sum[0] < n  - 1) return false;
-    ans[1] = *vec[start.first][start.second].begin();
-    vis[ans[1]] = 1;
-    
-    for (int i = 2; i <= n; ++i) {
-        std::set<int>::iterator it;
-        start.first ^= 1;
-        while (true) {
-            int idx = mp[a[start.first][ans[i - 1]]];
-            it = vec[start.first][idx].begin();
-            if (vec[start.first][idx].empty()) 
-                return false;
-            if (vis[*it]) vec[start.first][idx].erase(it);
-            else break;
-        }
-        ans[i] = *it;
-        vis[ans[i]] = 1;
-    }
+#include <bits/stdc++.h>
 
-    std::cout << "YES\n";
-    for (int i = 1; i <= n; ++i) std::cout << ans[i] << ' ';
-    std::cout << '\n';
-    return true;
+using namespace std;
+
+vector<set<int>> graph;
+vector<int> ans;
+
+void dfs(int v) {
+    while (!graph[v].empty()) {
+        int u = *graph[v].begin();
+        graph[u].erase(v);
+        graph[v].erase(u);
+        dfs(u);
+    }
+    ans.push_back(v);
 }
-int main() {
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-    #endif
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(NULL);
 
-    int T = 1;
-    std::cin >> T;
-    while (T--) {
-        std::cin >> n;
-        for (int i = 0; i < 2; ++i) {
-            vec[i].clear();
-            vec[i].resize(n * 2 + 1);
-        }
-        mp.clear();
-        count = 0;
+signed main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        graph.clear();
+        ans.clear();
+        int n;
+        cin >> n;
+        map<int, int> p, v;
+        map<pair<int, int>, int> toIndex;
+        vector<pair<int, int>> part;
         for (int i = 1; i <= n; ++i) {
-            int v, p;
-            std::cin >> v >> p;
-            if (!mp.count(v)) mp[v] = ++count;
-            vec[0][mp[v]].insert(i);
-            if (!mp.count(p)) mp[p] = ++count;
-            vec[1][mp[p]].insert(i);
-            a[0][i] = v;
-            a[1][i] = p;
-            vis[i] = 0;
+            int volume, pitch;
+            cin >> volume >> pitch;
+            toIndex[make_pair(volume, pitch)] = i;
+            if (p.count(pitch) == 0) {
+                p[pitch] = graph.size();
+                graph.push_back(set<int>());
+                part.push_back(make_pair(0, pitch));
+            }
+            if (v.count(volume) == 0) {
+                v[volume] = graph.size();
+                graph.push_back(set<int>());
+                part.push_back(make_pair(volume, 0));
+            }
+            graph[v[volume]].insert(p[pitch]);
+            graph[p[pitch]].insert(v[volume]);
         }
-        if(!solve()) std::cout << "NO\n";
+        int root = 0;
+        int cnt = 0;
+        for (int i = 0; i < graph.size(); ++i) {
+            if (graph[i].size() % 2 == 1) {
+                ++cnt;
+                root = i;
+            }
+        }
+        dfs(root);
+        if (ans.size() != n + 1 || cnt > 2) {
+            cout << "NO" << endl;
+            continue;
+        }
+        vector<int> out;
+        for (int i = 0; i < n; ++i) {
+            auto p1 = part[ans[i]];
+            auto p2 = part[ans[i + 1]];
+            out.push_back(toIndex[make_pair(p1.first + p2.first, p1.second + p2.second)]);
+        }
+        cout << "YES" << endl;
+        for (int i = 0; i < n; ++i) {
+            cout << out[i] << " ";
+        }
+        cout << endl;
     }
-
     return 0;
 }
