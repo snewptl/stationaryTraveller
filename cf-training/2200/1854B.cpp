7007bf49f6f33d68600b74ac0920863fd9b0e126
何卓霖 <1224430363@qq.com>
2025-06-07 19:42:12 +0800

    AC on 1854B

--- cf-training/2200/1854B.cpp
+++ cf-training/2200/1854B.cpp
@@ -5,8 +5,9 @@
 #include <set>
 #include <queue>
 #include <vector>
+#include <bitset>
+#include <memory.h>
 #include <assert.h>
-#include <cmath>
 #define all(x) x.begin(), x.end()
 typedef long long ll;
 typedef std::pair<int, int> pii;
@@ -17,17 +18,9 @@ typedef long double ldb;
 const int maxn = 2e5 + 5;
 const ll mod = 998244353;
 int n, a[maxn];
-std::set<pii> s[2], t;
+std::bitset<200001> dp, mask; 
 ll sum[maxn];
-int vis[maxn];
-int available[maxn];
-int solve() {
-    if (s[0].empty()) return 0;
-    for (int i = 1; i <= n; ++i) {
-        auto it = s[0].lower_bound({i + 1, 0});
-        if (it == s[0].begin()) continue;
-    }
-}
+ll ans;
 int main() {
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
@@ -37,71 +30,27 @@ int main() {
     std::cin.tie(NULL);
 
     int T = 1;
-    std::cin >> T;
+    // std::cin >> T;
     while (T--) {
         std::cin >> n;
         for (int i = 1; i <= n; ++i) {
-            std::cin >> a[i];
-            sum[i] = a[i] + sum[i - 1];
+            std::cin >> a[i];  
+            sum[i] = sum[i - 1] + a[i]; 
         }
-        s[0].clear();
-        s[1].clear();
-        t.clear();
-        s[0].insert({a[1], a[1]});
-        vis[a[1]] = 1;
-        for (int i = 2; i <= n; ++i) {
-            if (vis[a[i]]) {
-                if(!s[0].empty()) {
-                    auto it = s[0].end();
-                    --it;
-                    if (it->second < i - 1) {
-                        continue;
-                    }
-                    if (it->first + a[i] <= it->second + 1) {
-                        pii cur = {std::max(it->first, i - 1), std::min(it->second + a[i], n)};
-                        s[0].erase(it);
-                        s[0].insert(cur);
-                    } else {
-                        if (it->first + a[i] <= n) {
-                            s[0].insert(std::max(it->first + a[i], i - 1), std::min(it->second + a[i], n));
-                        }
-                    }
-                }
-                continue;
-            } 
-            vis[a[i]] = 1;
-            for (auto p : s[0]) {
-                if (p.second >= i - 1) {
-                    s[1].insert(p);
-                    if (p.first + a[i] <= n) {
-                        s[1].insert({p.first + a[i], std::min(n, p.second + a[i])});
-                    }
-                } else {
-                    t.insert(p);
-                }
+        ans = 0;
+        dp.reset();
+        dp.set(1);
+        mask.set();
+        mask <<= 1;
+        for (int i = 1; i <= n * 2; ++i) {
+            if (i <= n) dp |= dp << a[i];
+            if ((dp & mask).test(i)) {
+                ans = std::max(ans, sum[(i > n ? n : i)] - i + 1);
             }
-            s[0].clear();
-            pii cur = {0, 0};
-            while (!s[1].empty()) {
-                auto cur = *s[1].begin();
-                s[1].erase(s[1].begin());
-                if (s[1].empty()) {
-                    s[0].insert(cur);
-                    break;
-                }
-                auto it = s[1].begin();
-                while (it->first <= cur.second + 1) {
-                    cur.second = std::max(cur.second, it->second);
-                    s[1].erase(it);
-                    if (s[1].empty()) break;
-                    it = s[1].begin();
-                } 
-                s[0].insert(cur);
-            }
+            dp.reset(i);
+            mask.reset(i);
         }
-        for (auto it : s[0]) t.insert(it);
-        
-        std::cout << solve() << '\n';
+        std::cout << ans << '\n';
     }
 
     return 0;
