8ade86e8c6bcf57dfd79d6ccfb2728784aa52070
pinkyhead <1224430363@qq.com>
2025-07-10 21:10:18 +0800

    AC on 1406

--- cf-training/2200/1406D.cpp
+++ cf-training/2200/1406D.cpp
@@ -16,7 +16,15 @@ typedef double db;
 typedef long double ldb;
 const int maxn = 2e5 + 5;
 const ll mod = 998244353;
-
+int n, q;
+ll a[maxn];
+ll delta[maxn];
+ll sum;
+ll cal(ll x) {
+    ll res = x / 2;
+    if (res * 2 < x) res += 1;
+    return res;
+}
 int main() {
     #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
@@ -25,10 +33,31 @@ int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(NULL);
 
-    int T = 1;
-    std::cin >> T;
-    while (T--) {
+    std::cin >> n;
+    for (int i = 1; i <= n; ++i) std::cin >> a[i];
+    for (int i = 1; i < n; ++i) {
+        delta[i] = a[i + 1] - a[i];
+        sum += std::max(delta[i], 0ll);
+    }
+    std::cout << cal(sum + a[1]) << '\n';
+    std::cin >> q;
+    while (q--) {
+        int l, r, x;
+        std::cin >> l >> r >> x;
+        a[l] += x;
+        if (l > 1) {
+            sum -= std::max(delta[l - 1], 0ll);
+            delta[l - 1] += x;
+            sum += std::max(delta[l - 1], 0ll);
+        }
+        if (r < n) {
+            sum -= std::max(delta[r], 0ll);
+            delta[r] -= x;
+            sum += std::max(delta[r], 0ll);
+        }
+        std::cout << cal(sum + a[1]) << '\n';
     }
 
     return 0;
 }
+// 20 : 38 - 21 : 06
