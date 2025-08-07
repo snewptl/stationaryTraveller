33593f2a6c8fe828d6774ef8ea2a38d9784f3866
pinkyhead <1224430363@qq.com>
2025-08-07 16:31:48 +0800

    update

--- cf-training/2300/1988E.cpp
+++ cf-training/2300/1988E.cpp
@@ -1,36 +1,86 @@
-#include <algorithm>
-#include <cstdio>
-#include <iostream>
-#include <map>
-#include <set>
-#include <queue>
-#include <vector>
-#include <assert.h>
-#include <memory.h>
-#include <cmath>
-#define all(x) x.begin(), x.end()
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
 typedef long long ll;
-typedef std::pair<int, int> pii;
-typedef std::pair<ll, int> pli;
-typedef std::pair<ll, ll> pll;
-typedef double db;
-typedef long double ldb;
-const int maxn = 2e5 + 5;
-const ll mod = 998244353;
-
-int main() {
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    // freopen("output.txt", "w", stdout);
-    #endif
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(NULL);
-
-    int T = 1;
-    std::cin >> T;
-    while (T--) {
+ 
+const int N = 5e5 + 10;
+int a[N], pos[N];
+ll ans[N];
+ 
+void update(int l, int r, ll val) {
+    if (l <= r) {
+        ans[l] += val;
+        ans[r + 1] -= val;
     }
-
-    return 0;
 }
-// 0 : 02 - 
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+#ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+#endif
+ 
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i];
+        }
+ 
+        for (int i = 1; i <= n; i++) {
+            ans[i] = 0;
+            pos[a[i]] = i;
+        }
+ 
+        set<int> s;
+        s.insert(0);
+        s.insert(n + 1);
+        for (int val = 1; val <= n; val++) {
+            int i = pos[val];
+            auto rig_it = s.lower_bound(i);
+            int rig = *rig_it;
+            auto lef_it = rig_it;
+            lef_it--;
+            int lef = *lef_it;
+            if (lef >= 2) { // delete [1, lef - 1]
+                update(1, lef - 1, 1ll * (i - lef) * (rig - i) * a[i]);
+            }
+            if (rig <= n - 1) { // delete [rig + 1, n]
+                update(rig + 1, n, 1ll * (i - lef) * (rig - i) * a[i]);
+            }
+            { // delete [lef + 1, i - 1]
+                update(lef + 1, i - 1, 1ll * (i - lef - 1) * (rig - i) * a[i]);
+            }
+            { // delete [i + 1, rig - 1]
+                update(i + 1, rig - 1, 1ll * (i - lef) * (rig - i - 1) * a[i]);
+            }
+            if (lef >= 1) { // delete [lef]
+                lef_it--;
+                int x = *lef_it;
+                update(lef, lef, 1ll * (i - x - 1) * (rig - i) * a[i]);
+            }
+            if (rig <= n) { // delete [rig]
+                rig_it++;
+                int x = *rig_it;
+                update(rig, rig, 1ll * (i - lef) * (x - i - 1) * a[i]);
+            }
+            s.insert(i);
+        }
+ 
+        for (int i = 1; i <= n; i++) {
+            ans[i] += ans[i - 1];
+        }
+        for (int i = 1; i <= n; i++) {
+            cout << ans[i] << " ";
+        }
+        cout << "\n";
+    }
+ 
+#ifdef LOCAL
+    cout << "\nTime elapsed: " << double(clock()) / CLOCKS_PER_SEC << " s.\n";
+#endif
+}
