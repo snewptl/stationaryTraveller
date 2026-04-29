32ed51824021c6d6eb5414115b2a1a72f2ab9280
pinkyHead <1224430363@qq.com>
2026-04-29 12:03:39 +0800

    update

--- cf-round/2226/E.cpp
+++ cf-round/2226/E.cpp
@@ -61,6 +61,7 @@ void update(int rt, int l, int r, int xl, int xr, int delta) {
     push_up(rt);
 }
 int query(int rt, int l, int r, int xl, int xr) {
+    if (xr < xl) return 0;
     int mid = (l + r) / 2;
     if (xl == l && xr == r) {
         return t[rt].min;
@@ -84,30 +85,24 @@ int main() {
     while (T--) {
         std::cin >> n;
         build(1, 0, n);
-        int last = 0;
-        update(1, 0, n, 0, 0, -1);
+        int last = -1;
         for (int i = 1; i <= n; ++i) {
             std::cin >> a[i];
             all[a[i]] += 1;
-            update(1, 0, n, 0, std::min((last - 1) / 2, n), 1);
+            if (a[i] == last && all[a[i]] == 1) {
+                update(1, 0, n, 0, a[i], 1);
+            } else {
+                update(1, 0, n, 0, std::min((a[i] - 1) / 2, n), 1);
+            }
             while (query(1, 0, n, 0, last) >= 0) {
+                last += 1;
                 update(1, 0, n, 0, last, -1);
                 if (all[last]) {
                     update(1, 0, n, 0, (last - 1) / 2, -1);
                     update(1, 0, n, 0, last, 1);
                 }
-                last += 1;
-                if (query(1, 0, n, 0, last) < 0) {
-                    last -= 1;
-                    update(1, 0, n, 0, last, 1);
-                    if (all[last]) {
-                        update(1, 0, n, 0, (last - 1) / 2, 1);
-                        update(1, 0, n, 0, last, -1);
-                    }
-                    break;
-                }
             }
-            std::cout << last + 1 << ' ';
+            std::cout << last << ' ';
         }
         std::cout << '\n';
         for (int i = 1; i <= n; ++i) {
